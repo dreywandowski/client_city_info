f7cc214e37f03cd22f20450f50e66cd7c5585d5f
dreywandowski <aduramimo@gmail.com>
2024-07-01 21:06:45 +0100

    test

--- src/hng/hng.controller.spec.ts
+++ src/hng/hng.controller.spec.ts
@@ -1,5 +1,7 @@
 import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { HngController } from './hng.controller';
+import { HngModule } from './hng.module';
 import { HngService } from './hng.service';
 
 describe('HngService', () => {
@@ -7,8 +9,7 @@ describe('HngService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [ConfigModule.forRoot()],
-      providers: [HngService],
+      imports: [ConfigModule.forRoot(), HngModule],
     }).compile();
 
     service = module.get<HngService>(HngService);
@@ -16,5 +17,21 @@ describe('HngService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+});
+
+describe('HngController', () => {
+  let controller: HngController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot(), HngModule],
+    }).compile();
+
+    controller = module.get<HngController>(HngController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
   });
 });
